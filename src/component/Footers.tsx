94866228e47a1307773fe24d0af142e2a74d6037
Victor Alomia <alomia18@gmail.com>
2024-08-15 11:56:04 -0500

    configuracion rutas y cracioon header

--- src/component/Footers.tsx
+++ src/component/Footers.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
-
+import '../style/headers.css';
 export const Footers = () => {
   return (
-    <div>Footers</div>
+    <div className='footer_container'>Footers</div>
   )
 }
--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -3,8 +3,11 @@ import logo from '../img/logoDISC_sinfondo.png'
 import Col from '../img/col.png';
 import Us from '../img/us.png';
 import '../style/headers.css';
+import { Outlet } from 'react-router-dom';
+import { Footers } from './Footers';
 export const Header = () => {
   return (
+    <>
     <div className='container_header'>
         <div className='container_logo'>
             <Link to={"/"}>
@@ -16,9 +19,9 @@ export const Header = () => {
          <div className='container_nav'>
             <nav className='container_nav--lin'>
              <Link className='navegations_links'  to={"/"}>Home</Link>
-             <Link className='navegations_links' to={"/"}>Services</Link>
-             <Link className='navegations_links' to={"/"}>About us</Link>
-             <Link className='navegations_links' to={"/"}>Conatct us</Link>
+             <Link className='navegations_links' to={"#"}>Services</Link>
+             <Link className='navegations_links' to={"#"}>About us</Link>
+             <Link className='navegations_links' to={"#"}>Conatct us</Link>
             </nav>
          </div>
          <div className='container_flags'>
@@ -26,10 +29,13 @@ export const Header = () => {
             <img className='flags' src={Col}/>
            </div>
            <div className='container_flags--contry'>
-            <img className='flags' src={Us}/>
+            <img className='flags usaflag' src={Us}/>
            </div>
          </div>
         </div>
     </div>
+    <Outlet/>
+    <Footers/>
+    </>
   )
 }
