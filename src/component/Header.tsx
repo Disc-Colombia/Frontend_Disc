0a3c981c09f039b5c3a793d25db79417d6bb7dc1
Victor Alomia <alomia18@gmail.com>
2024-08-16 11:01:22 -0500

    incoprporacion de paginas y creacion de rutas

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -8,18 +8,18 @@ import "../style/headers.css";
 import { Outlet } from "react-router-dom";
 import { Footers } from "./Footers";
 export const Header = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-
+  const [isMenuOpen, setIsMenuOpen] = useState(false);//controla estado del menu
+//Muestra el menu
   const toggleMenu = () => {
-    console.log("mostrando..");
     setIsMenuOpen(!isMenuOpen);
   };
+  //Cierra el menu
   const closeMenu = () => {
     setIsMenuOpen(false);
   };
   return (
     <>
-      <div className="container_header">
+      <div className="container_header line-bottom">
         <div className="container_logo">
           <Link to={"/"}>
             <img
@@ -38,18 +38,17 @@ export const Header = () => {
               <Link className="navegations_links" onClick={closeMenu} to={"/"}>
                 Home
               </Link>
-              <Link className="navegations_links" onClick={closeMenu} to={"#"}>
+              <Link className="navegations_links" onClick={closeMenu} to={"/services"}>
                 Services
               </Link>
-              <Link className="navegations_links" onClick={closeMenu} to={"#"}>
+              <Link className="navegations_links" onClick={closeMenu} to={"/aboutUs"}>
                 About us
               </Link>
-              <Link className="navegations_links" onClick={closeMenu} to={"#"}>
+              <Link className="navegations_links" onClick={closeMenu} to={"/contactUs"}>
                 Conatct us
               </Link>
             </nav>
-          </div>
-          <div className="container_flags">
+            <div className="container_flags">
             <div className="container_flags--contry">
               <img className="flags" src={Col} />
             </div>
@@ -57,7 +56,10 @@ export const Header = () => {
               <img className="flags usaflag" src={Us} />
             </div>
           </div>
+          </div>
+        
         </div>
+      
       </div>
       <Outlet />
       <Footers />
