f1d5fc3479b95afa4e81d5c81acd8dc4d5c492cb
Victor Alomia <alomia18@gmail.com>
2024-08-30 20:11:15 -0500

    realizando cambios en el home  cambio imagen por carrousel

--- src/component/Footers.tsx
+++ src/component/Footers.tsx
@@ -1,9 +1,11 @@
 import logo from "../img/logoDISC_sinfondo.png";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "../style/headers.css";
+
 export const Footers = () => {
   return (
     <div className="footer_container">
+      
       <div className="footer_logo">
         <div className="containe_section--logo">
           <div className="footer_container--logo">
--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,17 +1,24 @@
-import { Home } from '../component/Home';
-import logo from '../img/logoDISC_sinfondo.png';
-import'../style/index.css';
+import { Home } from "../component/Home";
+import logo from "../img/logoDISC_sinfondo.png";
+import ColorLines from "../component/ColorLine";
+import "../style/index.css";
+import { Carrousel } from "../component/Carrousel";
 export const Index = () => {
   return (
-<>
-<div className='container_index'>
-  <div className='container_index--tittle'>
-    <img className='tittle_index--img' src={logo} alt="logo develoment" />
-    <h2 className='text_index--tittle'>Innovative <span className='tittle_index--span'>Results</span> </h2>
-  </div>
-  <div rel="preload" className='container_background'></div>
-      <Home/>
-    </div>
-</>
-  )
-}
+    <>
+      <div className="container_index">
+        <div className="container_index--tittle">
+          <img className="tittle_index--img" src={logo} alt="logo develoment" />
+          <h2 className="text_index--tittle">
+            Innovative <span className="tittle_index--span">Results</span>{" "}
+          </h2>
+        </div>
+        <div rel="preload" className="container_background container_carrousell--index">
+          <Carrousel/>
+        </div>
+        <ColorLines />
+        <Home />
+      </div>
+    </>
+  );
+};
