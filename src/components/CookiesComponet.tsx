8da01d87623c091d4204b0ac543f78372feea34e
Victor Alomia <alomia18@gmail.com>
2024-09-12 11:49:36 -0500

    implementacion de cookies finalizado

---
+++ src/components/CookiesComponet.tsx
@@ -0,0 +1,35 @@
+import { useEffect } from "react";
+import Cookies from "js-cookie";
+import "../styles/cookies.css";
+import { Dispatch } from "react";
+type CookiesProps = {
+  isVisible: boolean;
+  setIsVisible: Dispatch<React.SetStateAction<boolean>>;
+};
+export const CookiesComponet = ({ isVisible, setIsVisible }: CookiesProps) => {
+  // Verifica si la cookie de acceptation ya existe
+  useEffect(() => {
+    const cookiesAccepted = Cookies.get("cookiesAccepted");
+    if (cookiesAccepted) {
+      console.log("Cookie Accepted:", cookiesAccepted);
+      setIsVisible(true); // Mostrar banner si la cookie no existe
+    } else {
+      console.log("Cookie Accepted:", cookiesAccepted);
+      setIsVisible(false); // Ocultar banner si la cookie ya existe
+    }
+  }, [setIsVisible]);
+
+  const handleAccept = () => {
+    // Establece una cookie que expira en 365 días
+    Cookies.set("cookiesAccepted", "true", { expires: 365 });
+    
+    setIsVisible(true);
+    if (!isVisible) return null;
+  };
+  return (
+    <div className="cookie-banner">
+      Este sitio web utiliza cookies para mejorar tu experiencia.
+      <button onClick={handleAccept}>Aceptar</button>
+    </div>
+  );
+};
--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Home } from "../components/Home";
 import logo from "../imgs/logoDISC_sinfondo.png";
 import ColorLines from "../components/ColorLine";
@@ -5,26 +6,37 @@ import "../styles/index.css";
 import { Carrousel } from "../components/Carrousel";
 import { Services } from "../components/Services";
 import { AboutUs } from "../components/AboutUs.tsx";
-import {ContactUs} from "../components/ContactUs"
+import { ContactUs } from "../components/ContactUs";
+import { CookiesComponet } from "../components/CookiesComponet";
 
 export const Index = () => {
+  const [isVisible, setIsVisible] = useState<boolean>(false);
   return (
     <>
-      <div className="container_index"id="home" >
+      <div className="container_index" id="home">
         <div className="container_index--tittle">
           <img className="tittle_index--img" src={logo} alt="logo develoment" />
           <h2 className="text_index--tittle">
             Innovative <span className="tittle_index--span">Results</span>{" "}
           </h2>
         </div>
-        <div rel="preload" className="container_background container_carrousell--index">
-           <Carrousel/>
-          <ColorLines /> 
+        <div
+          rel="preload"
+          className="container_background container_carrousell--index"
+        >
+          {!isVisible && (
+            <CookiesComponet
+              isVisible={isVisible}
+              setIsVisible={setIsVisible}
+            />
+          )}
+          <Carrousel />
+          <ColorLines />
           <Home />
         </div>
-        <Services/>
-        <AboutUs/>
-        <ContactUs/>
+        <Services />
+        <AboutUs />
+        <ContactUs />
       </div>
     </>
   );
