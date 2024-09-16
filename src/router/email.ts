bd4254d5d24b6cf9b33811dd518df05669bc8451
anvorja <borja.carlos@correounivalle.edu.co>
2024-09-16 05:02:43 -0500

    Dockerfile added and workflow with GithubActions

--- src/router/email.ts
+++ src/router/email.ts
@@ -3,6 +3,9 @@ import type { EmailProps } from '../type/type';
 
 const dataEmail = async (data:EmailProps) => {
     console.log("Datos enviados", data)
+    console.log("url del backend", `${import.meta.env.VITE_API_URL}`)
+    console.log("api site", `${import.meta.env.VITE_APP_SITE_KEY}`)
+
     try {
         const response = await axios.post(
             `${import.meta.env.VITE_API_URL}/send-email`,
