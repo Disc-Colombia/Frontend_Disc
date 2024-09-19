79e38330f9252f476a27e9548a8a26064607cd96
Victor Alomia <alomia18@gmail.com>
2024-09-19 15:36:32 -0500

    finalizando boton flotante

--- src/components/ContactButton.tsx
+++ src/components/ContactButton.tsx
@@ -1,22 +1,18 @@
 import "../styles/contactButton.css";
-import 'bootstrap-icons/font/bootstrap-icons.css';
+import "bootstrap-icons/font/bootstrap-icons.css";
 
-type ButtomProps ={
-  
-  handleShowForm: () => void
-}
-export const ContactButton = ({handleShowForm}:ButtomProps) => {
-  const handleContact=()=>{
-   handleShowForm();
-  }
+type ButtomProps = {
+  handleShowForm: () => void;
+};
+export const ContactButton = ({ handleShowForm }: ButtomProps) => {
+  const handleContact = () => {
+    handleShowForm();
+  };
   return (
     <div className="contact_button">
       <div className="floating_button">
-      <i  className="floating_buttonbi bi-person-circle"
-      onClick={handleContact}
-      ></i>
-          {/* <i className="floating_button bi bi-marker-tip"></i> */}
-       
+        <i className="floating_button bi bi-wechat" onClick={handleContact}></i>
+        {/* <i className="floating_button bi bi-marker-tip"></i> */}
       </div>
     </div>
   );
