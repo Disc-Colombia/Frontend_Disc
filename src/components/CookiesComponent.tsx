887d914a633fe1249d38c797fa3bb5796ee3a123
Victor Alomia <alomia18@gmail.com>
2024-10-01 23:19:39 -0500

    iniciando componente de configuracion de cookies

--- src/components/CookiesComponent.tsx
+++ src/components/CookiesComponent.tsx
@@ -3,13 +3,14 @@ import { useEffect } from "react";
 import Cookies from "js-cookie";
 import "../styles/cookies.css";
 import { Dispatch } from "react";
-
+import CookiesPolicy from "./CookiesPolicy";
 type CookiesProps = {
   isVisible: boolean;
   setIsVisible: Dispatch<React.SetStateAction<boolean>>;
 };
 
 export const CookiesComponent = ({ isVisible, setIsVisible }: CookiesProps) => {
+  const [configuration, setConfiguration] = React.useState(false);
   // Verifica si la cookie de acceptation ya existe
   useEffect(() => {
     const cookiesAccepted = Cookies.get("cookiesAccepted");
@@ -21,30 +22,51 @@ export const CookiesComponent = ({ isVisible, setIsVisible }: CookiesProps) => {
   }, [setIsVisible]);
 
   const handleAccept = () => {
-  
     Cookies.set("cookiesAccepted", "true", { expires: 365 });
     setIsVisible(true);
     if (!isVisible) return null;
   };
   const rejectCookies = () => {
-    console.log("Cookies Rechazadas")
-    Cookies.remove('cookiesConsent');
+    console.log("Cookies Rechazadas");
+    Cookies.remove("cookiesConsent");
     setIsVisible(true);
     // Aquí puedes eliminar cookies opcionales o evitar que se carguen scripts
   };
+  const handleConfiguration = () => {
+    setConfiguration(true);
+  };
   return (
-    <div className="cookie-banner">
-      <p className="cookies_information">
-          This website uses cookies to improve your experience, provide
-          personalized content, and analyze site traffic. By clicking 'Accept',
-          you agree to the use of all cookies. You can manage your preferences by
-          clicking 'Cookie Settings.
-      </p>
-      <button className="aceptcookies" value="acept" onClick={handleAccept}>Accept</button>
-      <button className="rejectcookies" value="reject" onClick={rejectCookies}>Reject</button>
-      <div>
-      <button className="configuraton_cookies"> Configuracion de Cookies</button>
-      </div>
-    </div>
+    <>
+      {configuration ? (
+        <CookiesPolicy />
+      ) : (
+        <div className="cookie-banner">
+          <p className="cookies_information">
+            This website uses cookies to improve your experience, provide
+            personalized content, and analyze site traffic. By clicking
+            'Accept', you agree to the use of all cookies. You can manage your
+            preferences by clicking 'Cookie Settings.
+          </p>
+          <button className="aceptcookies" value="acept" onClick={handleAccept}>
+            Accept
+          </button>
+          <button
+            className="rejectcookies"
+            value="reject"
+            onClick={rejectCookies}
+          >
+            Reject
+          </button>
+          <div>
+            <button className="configuraton_cookies"
+            onClick={handleConfiguration}
+            >
+              {" "}
+              Configuracion de Cookies
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
