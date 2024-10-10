e1cad5ac698b7100a11dbb98eab0282bd8671f1f
anvorja <borja.carlos@correounivalle.edu.co>
2024-10-10 03:29:28 -0500

    Backend request for storage cookies

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,8 +1,19 @@
 /// <reference types="vite/client" />
+// declare global {
+//     interface Window {
+//         dataLayer: any[];  // Declaración global para que TypeScript lo reconozca
+//         gtag: (...args: any[]) => void;
+//     }
+// }
+//
+// export {};
+
+/// <reference types="vite/client" />
+
 declare global {
     interface Window {
-        dataLayer: any[];  // Declaración global para que TypeScript lo reconozca
-        gtag: (...args: any[]) => void;
+        dataLayer: Array<{ [key: string]: any }>;  // Tipo más específico para dataLayer
+        gtag: (...args: Array<string | number | object>) => void;  // Tipo más específico para gtag
     }
 }
 
