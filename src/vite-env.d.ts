ee6496f03b3c70d9871eab98e80ef92ea6c2d63f
anvorja <borja.carlos@correounivalle.edu.co>
2024-10-11 02:29:10 -0500

    .envs were fixed

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,9 +1,18 @@
 /// <reference types="vite/client" />
 
+// declare global {
+//     interface Window {
+//         dataLayer: any[]
+//         gtag: (...args: any[]) => void
+//     }
+// }
+//
+// export {}
+
 declare global {
     interface Window {
-        dataLayer: any[]
-        gtag: (...args: any[]) => void
+        dataLayer: (string | Date | Record<string, unknown>)[][]
+        gtag: (...args: (string | Date | Record<string, unknown>)[]) => void
     }
 }
 
