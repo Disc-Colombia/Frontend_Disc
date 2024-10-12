130e49d9b87398f37105fc2b22dc4463026ac823
anvorja <borja.carlos@correounivalle.edu.co>
2024-10-12 02:56:41 -0500

    Update cookies

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import { Header } from "./components/Header";
 import { Index } from "./pages/Index";
 import { ModalDemo } from "./pages/ModalDemo";
 import { AllServices} from "./pages/AllServices";
+import { CookiesPolicy } from "./pages/CookiesPolicy";
 import { AboutUs } from "./pages/AboutUs";
 
 const router = createBrowserRouter([
@@ -29,6 +30,10 @@ const router = createBrowserRouter([
       },{
         path:"/AllServices",
         element: <AllServices/>
+      },
+      {
+        path: "/cookiesPolicy/",
+        element: <CookiesPolicy />,
       },
       {
         // path:"/Human-Service",
