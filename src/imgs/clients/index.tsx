705c4eb00689d157cabb3e563042b9a00592d276
Deisy <deisy.melo@correounivalle.edu.co>
2024-10-14 22:57:23 -0500

    update png to jpg format

--- src/imgs/clients/index.tsx
+++ src/imgs/clients/index.tsx
@@ -1,15 +1,15 @@
-import client1 from '../clients/client_1b.webp';
-import client2 from '../clients/client_2b.webp';
-import client3 from '../clients/client_3.webp';
-import client4 from '../clients/client_4rr.webp';
-import client5 from '../clients/client_5.webp';
-import client6 from '../clients/client_6d.webp';
-import client7 from '../clients/client_7.webp';
-import client8 from '../clients/client_8c.webp';
-import client9 from '../clients/client_9b.webp';
-import client10 from '../clients/client_10.webp';
-import client11 from '../clients/client_11.webp';
-import client12 from '../clients/client_12.webp';
+import client1 from '../clients/client_1b.jpg';
+import client2 from '../clients/client_2b.jpg';
+import client3 from '../clients/client_3.jpg';
+import client4 from '../clients/client_4rr.jpg';
+import client5 from '../clients/client_5.jpg';
+import client6 from '../clients/client_6d.jpg';
+import client7 from '../clients/client_7.jpg';
+import client8 from '../clients/client_8c.jpg';
+import client9 from '../clients/client_9b.jpg';
+import client10 from '../clients/client_10.jpg';
+import client11 from '../clients/client_11.jpg';
+import client12 from '../clients/client_12.jpg';
 
 export {
     client1,
